c64aab55d372468f145ca6c1f90dd30684f3a4c8
pmswabeeh <pmswabeeh112@gmail.com>
2025-11-14 15:32:58 +0530

    BaseConversion

--- BaseConversion.java
+++ BaseConversion.java
@@ -1,23 +1,11 @@
 import java.util.Scanner;
-
 public class BaseConversion {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        
-        System.out.print("Enter an integer: ");
-        int number = scanner.nextInt();
-
-
-        String binary = Integer.toBinaryString(number);
-        String octal = Integer.toOctalString(number);
-        String hexadecimal = Integer.toHexString(number).toUpperCase();
-
-
-        System.out.println("Binary: " + binary);
-        System.out.println("Octal: " + octal);
-        System.out.println("Hexadecimal: " + hexadecimal);
-
-        scanner.close();
-    }
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+System.out.print("Enter an integer: ");
+int num = sc.nextInt();
+System.out.println("Decimal: " + num);
+System.out.println("Octal: " + Integer.toOctalString(num));
+System.out.println("Hexadecimal: " + Integer.toHexString(num).toUpperCase());
+}
 }
